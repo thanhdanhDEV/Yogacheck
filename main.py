293601ce41c4812210d22eecd394e5c1640cd291
Cao Thanh Danh <caothanhdanh.developer.1602@gmail.com>
2022-08-02 01:23:12 +0700

    Finish mask yoga pose

--- main.py
+++ main.py
@@ -21,15 +21,17 @@ STANDARD_ANGLE_POSE = []
 # T_POSE = {'elbow_right' : [165, 175], 'elbow_left' : [165, 175], 'shoulder_right' : [85, 105], 'shoulder_left' : [85, 105], 'hip_right' :[170, 180], 'hip_left' : [170, 180], 'knee_right' : [170, 180], 'knee_left' : [170, 180]}
 T_POSE = [[160, 175], [160, 175],[70, 105], [70, 105], [170, 180],[170, 180], [170, 180], [170, 180]]
 
-FORWARD_BAND = [[160, 180], [160, 180],[100, 170], [100, 170], [15, 100],[15, 100], [160, 180], [160, 180]]
-FORWARD_BAND_1 = [[160, 180], [160, 180],[100, 170], [100, 170], [15, 25],[15, 25], [160, 180], [160, 180]]
-FORWARD_BAND_2 = [[160, 180], [160, 180],[100, 170], [100, 170], [25, 35],[25, 35], [160, 180], [160, 180]]
-FORWARD_BAND_3 = [[160, 180], [160, 180],[100, 170], [100, 170], [35, 55],[35, 55], [160, 180], [160, 180]]
+FORWARD_BAND_1 = [[160, 180], [160, 180],[100, 170], [100, 170], [15, 30],[15, 30], [160, 180], [160, 180]]
+FORWARD_BAND_2 = [[160, 180], [160, 180],[100, 170], [100, 170], [30, 40],[30, 40], [160, 180], [160, 180]]
+FORWARD_BAND_3 = [[160, 180], [160, 180],[100, 170], [100, 170], [40, 55],[40, 55], [160, 180], [160, 180]]
 FORWARD_BAND_4 = [[160, 180], [160, 180],[100, 170], [100, 170], [55, 100],[55, 100], [160, 180], [160, 180]]
-FORWARD_BAND_LIST = [FORWARD_BAND, FORWARD_BAND_1, FORWARD_BAND_2, FORWARD_BAND_3, FORWARD_BAND_4]
+FORWARD_BAND_LIST = [FORWARD_BAND_1, FORWARD_BAND_2, FORWARD_BAND_3, FORWARD_BAND_4]
 
 
-IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\i_forwardbend2.jpg')
+IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\yoga_easy.jpg')  ## Not FORWARD BEND - 0
+# IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\i_forwardbend.jpg') ## FORWARD BEND - 500
+# IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\i_forwardbend2.jpg') ## FORWARD BEND - 250
+
 BG_COLOR = (192, 192, 192)  # gray
 LIST_POST = ["NOSE", "LEFT_EYE_INNER", "LEFT_EYE", "LEFT_EYE_OUTER", "RIGHT_EYE_INNER", "RIGHT_EYE", "RIGHT_EYE_OUTER",
              "LEFT_EAR", "RIGHT_EAR", "MOUTH_LEFT", "MOUTH_RIGHT", "LEFT_SHOULDER", "RIGHT_SHOULDER", "LEFT_ELBOW",
@@ -49,6 +51,16 @@ def calculate_angle(a,b,c):
         angle = 360-abs(angle)
     return abs(angle) 
 
+def is_pose_in_POSTLIST(list_angle, POSE):
+    is_pose_in = True
+    for i in range(len(list_angle)):
+        if list_angle[i] > POSE[i][0] and list_angle[i] < POSE[i][1]:
+            continue
+        # print(i) #determine number incorrect coordinates
+        is_pose_in = False
+        break
+    return is_pose_in
+
 def pose_detection(list_angle, POSE_LIST):
     mask_list = [500, 375, 250, 125]
     is_yoga_pose = "FORWARD BEND"
@@ -56,26 +68,17 @@ def pose_detection(list_angle, POSE_LIST):
     color = (0, 255, 0)
     # print(POSE_LIST[0][0]) #debug - test
     # print(list_angle[0])
-    for i in range(len(list_angle)):
-        if list_angle[i] > POSE_LIST[0][i][0] and list_angle[i] < POSE_LIST[0][i][1]:
-            continue
-        # print(i) #determine number incorrect coordinates
-        is_yoga_pose = "Not FORWARD BEND"
-        mask = 0
-        color = (255, 0, 0)   # red in RGB
-        break
-    
-    if is_yoga_pose == "FORWARD BEND":
-        for j in range(1,len(POSE_LIST)):
-            for i in range(len(list_angle)):
-                if list_angle[i] > POSE_LIST[j][i][0] and list_angle[i] < POSE_LIST[0][i][1]:
-                    continue
-                print("i and j", i, j)
-                break
-            print("i and j", i, j)
+    for i in range(len(POSE_LIST)):
+        if is_pose_in_POSTLIST(list_angle, POSE_LIST[i]): 
+            is_yoga_pose = "FORWARD BEND"
+            mask = mask_list[i]
+            color = (0, 255, 0)
             break
-        mask = mask_list[j-1]
-        color = (0, 255, 0)   # red in RGB
+        else:
+            is_yoga_pose = "Not FORWARD BEND"
+    if is_yoga_pose == "Not FORWARD BEND":
+        color = (0, 0, 255)
+        mask = 0
     return is_yoga_pose, color, mask
 
 def normalized_to_pixel_coordinates(
@@ -145,7 +148,7 @@ if __name__ == '__main__':
             print(is_yoga_pose)
 
             # Print Pose_detection
-            cv2.putText(image, is_yoga_pose + str(mask), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+            cv2.putText(image, is_yoga_pose + str(": ") + str(mask), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
 
             annotated_image = image.copy()
             # Draw segmentation on the image.
@@ -243,7 +246,7 @@ if __name__ == '__main__':
                 print(lic1)
                 is_yoga_pose, color, mask = pose_detection(lic1, FORWARD_BAND_LIST)
                 print(is_yoga_pose)
-                cv2.putText(image, is_yoga_pose + str(mask), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+                cv2.putText(image, is_yoga_pose + str(": ") + str(mask), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
                 for i in range(len(lic1)):
                     cv2.putText(image, str(round(lic1[i],2)), 
                                 tuple(np.multiply(lic2[i], [640, 480]).astype(int)), 
